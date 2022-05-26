732376be2d7433458c256d34da273aa1a388122b
terwer <youweics@163.com>
2022-05-26 15:11:13 +0800

    Test chinse input

--- rocky-linux-vnc/config/my_profile.sh
+++ rocky-linux-vnc/config/my_profile.sh
@@ -6,4 +6,9 @@ alias la='ls -a'
 alias lla='ls -l -a'
 # echo "alias setup finished"
 
+# ibus
+export GTK_IM_MODULE=ibus
+export QT_IM_MODULE=ibus
+export XMODIFIERS=@im=ibus
+
 source /config/my_profile_auto.sh
